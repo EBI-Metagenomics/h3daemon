b911979b82ed51ff6f6e5d7044a23ef3d703fdf2
Danilo Horta <danilo.horta@pm.me>
2023-02-01 16:21:31 +0000

    update container images

--- h3daemon/images.py
+++ h3daemon/images.py
@@ -1,5 +1,5 @@
 __all__ = ["H3MASTER_IMAGE", "H3WORKER_IMAGE", "HMMPRESS_IMAGE"]
 
-H3MASTER_IMAGE = "quay.io/danilohorta/h3master"
-H3WORKER_IMAGE = "quay.io/danilohorta/h3worker"
-HMMPRESS_IMAGE = "quay.io/danilohorta/hmmpress"
+H3MASTER_IMAGE = "quay.io/microbiome-informatics/h3master"
+H3WORKER_IMAGE = "quay.io/microbiome-informatics/h3worker"
+HMMPRESS_IMAGE = "quay.io/microbiome-informatics/hmmpress"
