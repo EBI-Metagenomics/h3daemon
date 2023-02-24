8a7d4f6e89e1cea6421f160575e2bcb7e772fcf7
Danilo Horta <danilo.horta@pm.me>
2023-02-24 01:31:53 +0000

    No need for podman

--- h3daemon/cli.py
+++ h3daemon/cli.py
@@ -1,123 +1,98 @@
 from __future__ import annotations
 
 import importlib.metadata
-import json
+import os
+import signal
+import sys
 from pathlib import Path
 from typing import Optional
 
+import psutil
 import typer
-from rich.progress import Progress, SpinnerColumn
+from daemon import DaemonContext
+from daemon.pidfile import PIDLockFile
 from typer import echo
 
-from h3daemon.errors import PodInUseError
-from h3daemon.hmmfile import HMMFile
-from h3daemon.hmmpress import hmmpress
-from h3daemon.manager import H3Manager
-from h3daemon.namespace import Namespace
-from h3daemon.pod import H3Pod
+from h3daemon import H3Daemon
 
 __all__ = ["app"]
 
 
-app = typer.Typer(
-    add_completion=False,
-    pretty_exceptions_short=True,
-    pretty_exceptions_show_locals=False,
-)
+app = typer.Typer(add_completion=False)
+
+O_VERSION = typer.Option(None, "--version", is_eager=True)
+O_PORT = typer.Option(0, help="Port to listen to.")
+O_ALL = typer.Option(False, "--all")
 
 
 @app.callback(invoke_without_command=True)
-def cli(version: Optional[bool] = typer.Option(None, "--version", is_eager=True)):
+def cli(version: Optional[bool] = O_VERSION):
     if version:
         echo(importlib.metadata.version(__package__))
         raise typer.Exit()
 
 
 @app.command()
-def sys():
-    """
-    Show Podman information.
-    """
-    with H3Manager() as h3:
-        x = h3.sys()
-        echo(f"Release: {x.release}")
-        echo(f"Compatible API: {x.compatible_api}")
-        echo(f"Podman API: {x.podman_api}")
-
-
-@app.command()
-def info(namespace: str):
-    """
-    Show namespace information.
-    """
-    with H3Manager():
-        pod = H3Pod(namespace=Namespace(namespace))
-        echo(json.dumps(pod.info().asdict(), indent=2))
-
-
-@app.command()
-def stop(
-    namespace: Optional[str] = typer.Argument(None),
-    all: bool = typer.Option(False, "--all"),
-):
-    """
-    Stop namespace.
-    """
-    with Progress(SpinnerColumn(), transient=True) as progress:
-        progress.add_task(description="", total=None)
-        with H3Manager() as h3:
-            namespaces = []
-            if all:
-                assert not namespace
-                namespaces += h3.namespaces()
-            else:
-                assert namespace
-                namespaces.append(Namespace(namespace))
-
-            for ns in namespaces:
-                h3.stop_daemon(ns)
-
-
-@app.command()
-def ls():
-    """
-    List namespaces.
-    """
-    with H3Manager() as h3:
-        for ns in h3.namespaces():
-            echo(str(ns))
-
-
-@app.command()
-def start(
-    hmmfile: Path,
-    port: int = typer.Option(
-        0, help="Port to listen to. Randomly chooses one that is available if 0."
-    ),
-    force: bool = typer.Option(
-        False, "--force", help="Stop namespace first if it already exists."
-    ),
-):
+def start(hmmfile: Path, port: int = O_PORT):
     """
     Start daemon.
     """
-    with Progress(SpinnerColumn(), transient=True) as progress:
-        progress.add_task(description="", total=None)
-        with H3Manager() as h3:
-            try:
-                pod = h3.start_daemon(HMMFile(hmmfile), port, force)
-            except PodInUseError as excp:
-                echo(excp.msg)
-                raise typer.Exit(1)
-    echo(f"🎉 Daemon started listening at {pod.host_ip}:{pod.host_port}")
+    if not hmmfile.name.endswith(".hmm"):
+        raise ValueError(f"`{hmmfile}` does not end with `.hmm`.")
+
+    if not hmmfile.exists():
+        raise ValueError(f"`{hmmfile}` does not exist.")
+
+    extensions = ["h3f", "h3i", "h3m", "h3p"]
+    for x in extensions:
+        filename = Path(f"{hmmfile}.{x}")
+        if not filename.exists():
+            raise ValueError(f"`{filename.name}` must exist as well.")
+
+    workdir = str(hmmfile.parent)
+    pidfile = PIDLockFile(str(hmmfile) + ".pid")
+    with DaemonContext(working_directory=workdir, pidfile=pidfile):
+        daemon = H3Daemon(port, hmmfile.name)
+        daemon.run()
 
 
 @app.command()
-def press(hmmfile: Path):
+def stop(hmmfile: Optional[Path] = typer.Argument(None), all: bool = O_ALL):
     """
-    Press HMMER ASCII file.
+    Stop daemon.
     """
-    with Progress(SpinnerColumn(), transient=True) as progress:
-        progress.add_task(description="", total=None)
-        with H3Manager():
-            hmmpress(HMMFile(hmmfile))
+    if all:
+        stop_all()
+        return
+
+    assert hmmfile
+    if not hmmfile.name.endswith(".hmm"):
+        raise ValueError(f"`{hmmfile}` does not end with `.hmm`.")
+
+    if not hmmfile.exists():
+        raise ValueError(f"`{hmmfile}` does not exist.")
+
+    os.kill(read_pidfile(str(hmmfile) + ".pid"), signal.SIGTERM)
+
+
+def stop_all():
+    pid = os.getpid()
+    exe = sys.argv[0]
+    uid = os.getuid()
+
+    found = True
+    while found:
+        found = False
+        for proc in psutil.process_iter(["pid", "cmdline", "uids"]):
+            if proc.uids().real != uid or proc.pid == pid:
+                continue
+            cmdline = proc.cmdline()
+            if len(cmdline) > 1 and cmdline[1] == exe:
+                proc.terminate()
+                found = True
+                break
+
+
+def read_pidfile(pidfile):
+    with open(pidfile, "r") as file:
+        return int(file.readline().strip())
